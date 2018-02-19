0198317558b6c2b63cb5a3801bfad830d167119f
tbouma <tbouma@TrevorBouma.student.iastate.edu>
2018-02-19 14:57:04 -0600

    "Calculating StartPay from 5pm-10pm"

--- source/pillar/Babysitter.java
+++ source/pillar/Babysitter.java
@@ -1,12 +1,26 @@
 package pillar;
 public class Babysitter {
-	public String calcTotalPay(int start, int end) {
+	public int calcTotalPay(int start, int end, int bedtime) {
+	
+		int startRate = 12;
+		int startPay;
+		
+		//Babysitter can't start before 5pm (17 on 24hr clock)
 		if (start < 17) {
-			return "0";
+			return 0;
 		}
+		
+		/*Babysitter can't stay passed 4am (4 on 24hr clock)
+		 * 
+		 * (end < 17) to ensure program runs when babysitter leaves before 1am
+		 */
 		if (end > 4 && end < 17) {
-			return "0";
+			return 0;
 		}
-		return "1";
+		
+		//calculate start to bedtime pay
+		startPay = (bedtime - start) * startRate;
+		
+		return startPay;
 	}
 }
--- test/pillar/BabysitterTest.java
+++ test/pillar/BabysitterTest.java
@@ -9,7 +9,11 @@ public class BabysitterTest {
 	@Test
 	public void startNoEarlierThan5pmLeaveNoLaterThan4am() {
 		Babysitter babysitter = new Babysitter();
-		assertEquals("1", babysitter.calcTotalPay(17, 4));
+		assertEquals(0, babysitter.calcTotalPay(12, 4, 0));
 	}
-	
+	@Test 
+	public void whenStartingAt5pmAndBedtimeIs10pmCalculateStartPay() {
+		Babysitter babysitter = new Babysitter();
+		assertEquals(60, babysitter.calcTotalPay(17, 4, 22));
+	}
 }
